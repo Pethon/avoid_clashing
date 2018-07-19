69353f07e6252085e51e040ba132db4b753f19dd
pethon <pasin_th@hotmail.com>
2018-07-19 15:30:14 +0900

    change node structure

--- src/avoid_clashing_node.cpp
+++ src/avoid_clashing_node.cpp
@@ -3,18 +3,22 @@
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 
-geometry_msgs::Twist cmd_vel;
+geometry_msgs::Twist cmd_vel_stop;
 ros::Publisher pub_cmd_vel;
+bool obstacle = false;
 
-void callback_laser(const sensor_msgs::LaserScan::ConstPtr& msg){
+void callback_laser(const sensor_msgs::LaserScan::ConstPtr& laser_msg){
 
-	if(msg->ranges[msg->ranges.size()/2] <= 2){
-		//cmd_vel.angular.z = 5;
-		for(int i = 0; i < 4; i++){
-			pub_cmd_vel.publish(cmd_vel);
-			ROS_INFO("DANGER!!");
-			usleep(50*1000);
-		}
+	if(laser_msg->ranges[laser_msg->ranges.size()/2] <= 2) obstacle = true;
+	else obstacle = false;
+}
+
+void callback_move_base(const geometry_msgs::Twist::ConstPtr& move_base_cmd_vel){
+
+	if(obstacle == false) pub_cmd_vel.publish(move_base_cmd_vel);
+	else{
+		pub_cmd_vel.publish(cmd_vel_stop);
+		ROS_INFO("DANGER!!");
 	}
 }
 
@@ -25,6 +29,7 @@ int main(int argc, char **argv){
 	ros::NodeHandle n;
 
 	ros::Subscriber sub_laser = n.subscribe("/scan", 1, callback_laser);
+	ros::Subscriber sub_move_base = n.subscribe("/cmd_vel_test", 1, callback_move_base);
 
 	pub_cmd_vel = n.advertise<geometry_msgs::Twist>("/icart_mini/cmd_vel", 1);
 
