6fb299c246e5e5ec6f3cc5d0e61cabfded015122
pethon <pasin.theerapap@gmail.com>
2018-11-04 11:58:32 +0900

    add move_back recovery

--- src/avoid_clashing_node.cpp
+++ src/avoid_clashing_node.cpp
@@ -1,24 +1,46 @@
+#define DISTANCE 1.5f
+
 #include <unistd.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
 
 geometry_msgs::Twist cmd_vel_stop;
+geometry_msgs::Twist cmd_vel_back;
 ros::Publisher pub_cmd_vel;
 bool obstacle = false;
+double stop_time;
+double now_time;
 
 void callback_laser(const sensor_msgs::LaserScan::ConstPtr& laser_msg){
 
-	if(laser_msg->ranges[laser_msg->ranges.size()/2] <= 0.5) obstacle = true;
-	else obstacle = false;
+	if(laser_msg->ranges[laser_msg->ranges.size()/2] <= DISTANCE){
+		if(obstacle == false){
+			stop_time = ros::Time::now().toSec();
+		}
+		obstacle = true;
+	}
+	else{
+		obstacle = false;
+	}
 }
 
-void callback_move_base(const geometry_msgs::Twist::ConstPtr& move_base_cmd_vel){
+void callback_move_base(const geometry_msgs::Twist::ConstPtr& cmd_vel_from_move_base){
 
-	if(obstacle == false) pub_cmd_vel.publish(move_base_cmd_vel);
+	if(obstacle == true){
+		now_time = ros::Time::now().toSec();
+		if(now_time - stop_time < 3){
+			pub_cmd_vel.publish(cmd_vel_stop);
+			ROS_ERROR("OBSTACLE DETECTED!!");
+		}
+		else if(now_time - stop_time < 15){
+			cmd_vel_back.linear.x = -0.1;
+			pub_cmd_vel.publish(cmd_vel_back);
+			ROS_INFO("MOVE BACK!!");
+		}
+	}
 	else{
-		pub_cmd_vel.publish(cmd_vel_stop);
-		ROS_ERROR("DANGER!!");
+		pub_cmd_vel.publish(cmd_vel_from_move_base);
 	}
 }
 
